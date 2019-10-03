2cdd54e5a65d61706d7b8efac26223edd9cdacd3
guruwanabe <bp.stanciu@gmail.com>
2019-10-03 13:43:05 +0300

    Update unsplash.js

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const API_KEY = '717b57f84feb59495b219b25964907db50f4a94706dd0c0d2c8a559c3999a1c5';
-//export const SECRET = '330bce6cdbf478789452b158e04a3779b6da5ceb9eefb3cced0a56de985f4cdb';
 export const ROOT_URL = 'https://api.unsplash.com/';
 
 export default axios.create({
